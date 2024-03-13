b652055c4c13f440d2d3c67830338b7db0c8e69b
ClaudeMathys <claude.mathys@ksb-sg.ch>
2024-03-13 16:10:12 +0100

    Added FieldCaretaker class

---
+++ Minesweeper.Logic/FieldCaretaker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minesweeper.Logic
+{
+    internal class FieldCaretaker
+    {
+        private List<Memento> states = new List<Memento>();
+
+        public void PushState(Memento state)
+        {
+            states.Add(state);
+        }
+
+        public Memento Pop()
+        {
+            if (states.Count == 0) return null;
+
+            var lastIndex = states.Count - 1;
+            var lastState = states[lastIndex];
+            states.RemoveAt(lastIndex);
+
+            return lastState;
+        }
+    }
+}
